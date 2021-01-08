705c9a6826a4d2a2beb97937a82074bd4dab290c
overpowerhero <71799083+overpowerhero@users.noreply.github.com>
2021-01-08 10:13:05 +0800

    Initial commit

---
+++ Assets/prototype2-HYC ver/script/generateAnimal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateAnimal : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Ganimal", 1.0f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+
+           // Ganimal();
+        //}
+    }
+
+    void Ganimal() 
+    {
+
+        int animal_indexer = Random.Range(0, animalPrefabs.Length);
+        int x_pos = Random.Range(-18, 18);
+        Instantiate(animalPrefabs[animal_indexer], new Vector3(x_pos, 0, 25), animalPrefabs[animal_indexer].transform.rotation);
+
+
+
+    }
+}
---
+++ Assets/prototype2-HYC ver/script/outoffbound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class outoffbound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for food
+        if (transform.position.z > 40)
+        {
+            Destroy(gameObject);
+        }
+        //for dog
+        if (transform.position.z < -20)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
