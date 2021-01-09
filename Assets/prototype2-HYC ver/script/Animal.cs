7d82d46ee2f88ea9561e49ca2b029085611ef405
overpowerhero <71799083+overpowerhero@users.noreply.github.com>
2021-01-09 11:57:37 +0800

    Revert "Merge branch 'main' of https://github.com/overpowerhero/final-work-package into main"
    This reverts commit d9f1133601b918e38d973719dc6bb0a4c5c36587, reversing
    changes made to 7d2db34981a8639caf41b132a6b66c3c41859d72.

--- Assets/prototype2-HYC ver/script/Animal.cs
+++ Assets/prototype2-HYC ver/script/Animal.cs
@@ -8,25 +8,16 @@ using UnityEngine;
 
 public class Animal : MonoBehaviour
 {
-    public float speed = 5.0f;
-    private Rigidbody enemyRb;//lok 0108
-    private GameObject player;//lok 0108
-
+    public float speed = 20;
     // Start is called before the first frame update
     void Start()
     {
-        //lok 0108-HYC
-        enemyRb = GetComponent<Rigidbody>();
-        player = GameObject.Find("Human");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-
-        //lok 0108
-        Vector3 lookDirection = (player.transform.position- transform.position).normalized;
-        enemyRb.AddForce(lookDirection * speed);
     }
 }
