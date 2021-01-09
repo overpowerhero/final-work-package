a7339eb50f73542e5ade8965736c655ea6e88bb1
overpowerhero <71799083+overpowerhero@users.noreply.github.com>
2021-01-09 12:03:33 +0800

    test.1/09 12.03

--- Assets/prototype2-HYC ver/script/Animal.cs
+++ Assets/prototype2-HYC ver/script/Animal.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< HEAD
 // tset github
 // shiouuu test 這是琇1/09晚上傳的
 //HYC test 1/08 10pm
 
+=======
+>>>>>>> parent of 7492140... 1/08-10.23
 public class Animal : MonoBehaviour
 {
     public float speed = 20;
